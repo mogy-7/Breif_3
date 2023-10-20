8b35be112a8678425a3087f67b0cfa0658ed97a3
Nouaamane <mogynou@gmail.com>
2023-10-20 20:51:26 +0100

    begen

--- script.js
+++ script.js
@@ -1,9 +1,86 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
-    const menu = document.getElementById("menu");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const hamburger = document.getElementById("menu-icon");
+//     const menu = document.getElementById("menu");
     
-    menuIcon.addEventListener("click", function () {
-        menuIcon.classList.toggle("active");
-        menu.classList.toggle("active");
-    });
-});
+
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click",() =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+// contact side
+
+
+// const input = document.querySelector('input[name="subEmail"]');
+
+// const subForm = document.querySelector('#subscribeForm');
+// const subThankYou = document.querySelector('.subThankYou');
+// const subTitle = document.querySelector('.subTitle');
+
+// let isSubValid = false ;
+
+
+// const reseatSubscriptionInput = (elm) => {
+//     elm.classList.remove('subInvalid');
+//     elm.nextElementSibling.classList.add('hidden');
+//     elm.nextElementSibling.classList.remove('invalidBtn');
+// }
+
+
+// const validateSubscription = () => {
+//     isSubValid = true ;
+//     reseatSubscriptionInput(input);
+
+//     if (!input.value) {
+
+//         input.classList.add('subInvalid');
+//         input.classList.remove('valid');
+//         input.nextElementSibling.classList.remove('hidden');
+//         input.nextElementSibling.classList.add('invalidBtn');
+
+//         isSubValid = false ;
+//     }
+// }
+
+
+// subForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     validateSubscription();
+
+//     if (isSubValid == true) {
+//         subForm.remove();
+//         subTitle.classList.add('hidden');
+//         subThankYou.classList.remove('hidden');
+//     }
+// })
+
+
+// const validSubscription = (elm) => {
+//     elm.nextElementSibling.classList.add('hidden');
+//     elm.nextElementSibling.classList.remove('invalidBtn');
+
+//     elm.classList.remove('subInvalid');
+//     elm.classList.add('valid');
+// }
+
+
+// input.addEventListener('input', () => {
+//     validateSubscription();
+//     validSubscription(input);
+// })
+
+function submitFunction (){
+    open("contact.html");
+    
+}
